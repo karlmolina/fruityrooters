312ea3e8a03607d4cbc094880307b89c07440572
Karl Molina <karlgmolina@gmail.com>
2024-05-16 09:32:10 -0600

    Match all paths in navigo

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import './style.css'
 
-import { html } from 'htl'
 import Navigo from 'navigo'
 import home from './pages/home.ts'
 
 const navigo = new Navigo('/')
 
-navigo.on(() => {
-    document.body.replaceChildren(home())
-  })
+navigo.on('*', () => {
+  document.body.replaceChildren(home())
+})
 
 navigo.resolve()
 export default navigo
